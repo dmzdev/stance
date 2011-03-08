986a9980a7865564b3bd8577002350d34bc178b4
Scott Shillcock <scott@shillcock.net>
2011-03-08 13:25:21 -0800

    wip

--- scripts/login.js
+++ scripts/login.js
@@ -4,21 +4,22 @@ var dmz =
        , message: require("dmz/runtime/messaging")
        , defs: require("dmz/runtime/definitions")
        , objectType: require("dmz/runtime/objectType")
-       , mainWindow: require("dmz/ui/mainWindow")
+       , const: require("const")
+       , ui:
+          { mainWindow: require("dmz/ui/mainWindow")
+          }
        }
     // Constants
-    , UserType = dmz.objectType.lookup("user")
-    , NameAttr = dmz.defs.createNamedHandle("name")
-    , CookieAttr = dmz.defs.createNamedHandle("cookie")
-    , HILAttr = dmz.object.HILAttribute
+    , TimeStampAttr = dmz.defs.createNamedHandle("time-stamp")
     , LoginSuccessMessage = dmz.message.create("Login_Success_Message")
     , LogoutMessage = dmz.message.create("Logout_Message")
     // Variables
-    , _window = dmz.mainWindow.window()
+    , _window = dmz.ui.mainWindow.window()
     , _title = _window.title()
     , _userList = []
     , _userName
     , _userHandle
+    , _serverTime
     // Fuctions
     , _activateUser
     ;
@@ -31,14 +32,14 @@ self.shutdown = function () {
 
 _activateUser = function (name) {
 
-   if (_userHandle) { dmz.object.flag(_userHandle, HILAttr, false); }
+   if (_userHandle) { dmz.object.flag(_userHandle, dmz.object.HILAttribute, false); }
 
    if (_userName && (name === _userName)) {
 
       var handle = _userList[_userName];
       if (handle) {
 
-         dmz.object.flag(handle, HILAttr, true);
+         dmz.object.flag(handle, dmz.object.HILAttribute, true);
 
          _window.title(_title + " (" + _userName + ")");
          self.log.info("User identified: " + _userName);
@@ -49,9 +50,15 @@ _activateUser = function (name) {
 
 LoginSuccessMessage.subscribe(self, function (data) {
 
+   var timeStamp
+     ;
+
    if (data && dmz.data.isTypeOf(data)) {
 
-      _userName = data.string(NameAttr);
+      timeStamp = data.number(TimeStampAttr);
+      if (timeStamp) { _serverTime = new Date(timeStamp * 1000); }
+
+      _userName = data.string(dmz.const.NameHandle);
       _activateUser(_userName);
    }
 });
@@ -63,14 +70,21 @@ LogoutMessage.subscribe(self, function () {
 });
 
 
-dmz.object.text.observe(self, NameAttr, function (handle, attr, value) {
+//dmz.object.create.observe(self, function (handle, type) {
+
+//   if (type.isOfType(dmz.const.GameType)) {
+
+//      dmz.object.timestamp(handle, "MontereyTime", _serverTime);
+//   }
+//}
+
+
+dmz.object.text.observe(self, dmz.const.NameHandle, function (handle, attr, value) {
 
    var type = dmz.object.type(handle);
-
-   if (type && type.isOfType (UserType)) {
+   if (type && type.isOfType (dmz.const.UserType)) {
 
       _userList[value] = handle;
-
       _activateUser (value);
    }
 });
@@ -78,8 +92,7 @@ dmz.object.text.observe(self, NameAttr, function (handle, attr, value) {
 
 dmz.object.flag.observe(self, dmz.object.HILAttribute, function (handle, attr, value) {
 
-                           self.log.warn("HIL set");
-   var type = dmz.object.type(handle)
+   var type = dmz.object.type(handle);
 
    if (handle === _userHandle) {
 
@@ -90,7 +103,7 @@ dmz.object.flag.observe(self, dmz.object.HILAttribute, function (handle, attr, v
       }
    }
 
-   if (value && type && type.isOfType(UserType)) { _userHandle = handle; }
+   if (value && type && type.isOfType(dmz.const.UserType)) { _userHandle = handle; }
 });
 
 
