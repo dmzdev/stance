c734f79f7af0c4cb1e3030c7df5d84b444ef7e4d
Benjamin Sangster <benjamin.e.sangster@gmail.com>
2011-03-23 14:08:42 -0700

    Login fix.

--- scripts/devtools.js
+++ scripts/devtools.js
@@ -91,7 +91,7 @@ dmz.object.create.observe(self, function (handle, objType) {
 
       if (selected) {
 
-//         LogoutMessage.send();
+         LogoutMessage.send();
 
          handle = selected.data();
          username = selected.text();
