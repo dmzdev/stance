4fb2497c76da6ad2f16c1a79a50b8b7225aa8c55
Benjamin Sangster <benjamin.e.sangster@gmail.com>
2011-07-19 10:43:02 -0700

    Style adjustments, minor cleanup.

--- scripts/stanceConst.js
+++ scripts/stanceConst.js
@@ -8,102 +8,94 @@ var dmz =
    , _exports = {}
 
    , ObjectTypes =
-      { AdvisorType: dmz.objectType.lookup("advisor")
-      , EmailType: dmz.objectType.lookup("email")
-      , ForumType: dmz.objectType.lookup("forum")
-      , GameType: dmz.objectType.lookup("game")
-      , GroupType: dmz.objectType.lookup("group")
-      , LobbyistType: dmz.objectType.lookup("lobbyist")
-      , MemoType: dmz.objectType.lookup("media-memo")
-      , NewspaperType: dmz.objectType.lookup("media-newspaper")
-      , PinType: dmz.objectType.lookup("map_push_pin")
-      , PostType: dmz.objectType.lookup("post")
-      , CommentType: dmz.objectType.lookup("comment")
-      , QuestionType: dmz.objectType.lookup("question")
-      , AnswerType: dmz.objectType.lookup("answer")
-      , UserType: dmz.objectType.lookup("user")
-      , VideoType: dmz.objectType.lookup("media-video")
-      , VoteType: dmz.objectType.lookup("vote")
-      , DecisionType: dmz.objectType.lookup("decision")
-      }
+        { AdvisorType: dmz.objectType.lookup("advisor")
+        , AnswerType: dmz.objectType.lookup("answer")
+        , CommentType: dmz.objectType.lookup("comment")
+        , DecisionType: dmz.objectType.lookup("decision")
+        , EmailType: dmz.objectType.lookup("email")
+        , ForumType: dmz.objectType.lookup("forum")
+        , GameType: dmz.objectType.lookup("game")
+        , GroupType: dmz.objectType.lookup("group")
+        , LobbyistType: dmz.objectType.lookup("lobbyist")
+        , MemoType: dmz.objectType.lookup("media-memo")
+        , NewspaperType: dmz.objectType.lookup("media-newspaper")
+        , PinType: dmz.objectType.lookup("map_push_pin")
+        , PostType: dmz.objectType.lookup("post")
+        , QuestionType: dmz.objectType.lookup("question")
+        , UserType: dmz.objectType.lookup("user")
+        , VideoType: dmz.objectType.lookup("media-video")
+        , VoteType: dmz.objectType.lookup("vote")
+        }
 
    , Handles =
-      { ActiveHandle: dmz.defs.createNamedHandle("Active")
-      , AdminHandle: dmz.defs.createNamedHandle("admin_user")
-      , AdvisorGroupHandle: dmz.defs.createNamedHandle("advisor_group")
-      , BioHandle: dmz.defs.createNamedHandle("bio")
-      , CommentHandle: dmz.defs.createNamedHandle("comment")
-      , CreatedByHandle: dmz.defs.createNamedHandle("created_by")
-      , DisplayNameHandle: dmz.defs.createNamedHandle("display_name")
-      , DurationHandle: dmz.defs.createNamedHandle("duration")
-      , ForumLink: dmz.defs.createNamedHandle("group_forum_link")
-      , GameGroupHandle: dmz.defs.createNamedHandle("game_group")
-      , GameForumsHandle: dmz.defs.createNamedHandle("game_forums")
-//      , GameMediaHandle: dmz.defs.createNamedHandle("game_media")
-      , GameUngroupedUsersHandle: dmz.defs.createNamedHandle("game_ungrouped_users")
-      , GroupMembersHandle: dmz.defs.createNamedHandle("group_members")
-      , ID: dmz.defs.createNamedHandle("id")
-      , NameHandle: dmz.defs.createNamedHandle("name")
-      , ParentHandle: dmz.defs.createNamedHandle("parent")
-      , PictureHandle: dmz.defs.createNamedHandle("picture")
-      , EmailRecipientHandle: dmz.defs.createNamedHandle("email_recipient")
-      , SentHandle: dmz.defs.createNamedHandle("sent")
-      , TextHandle: dmz.defs.createNamedHandle("text")
-      , TitleHandle: dmz.defs.createNamedHandle("title")
-      , VisibleHandle: dmz.defs.createNamedHandle("visible")
-      , BackgroundImageHandle: dmz.defs.createNamedHandle("background_image")
-      , MapImageHandle: dmz.defs.createNamedHandle("map_image")
-      , ComputerImageHandle: dmz.defs.createNamedHandle("computer_image")
-      , TVImageHandle: dmz.defs.createNamedHandle("tv_image")
-      , NewspaperImageHandle: dmz.defs.createNamedHandle("newspaper_image")
-      , InboxImageHandle: dmz.defs.createNamedHandle("inbox_image")
-      , PhoneImageHandle: dmz.defs.createNamedHandle("phone_image")
-      , AdvisorImageHandle: dmz.defs.createNamedHandle("advisor_image")
-      , AdvisorImageCountHandle: dmz.defs.createNamedHandle("advisor_image_count")
-      , ExitImageHandle: dmz.defs.createNamedHandle("exit_image")
-      , ResourceImageHandle: dmz.defs.createNamedHandle("resource_image")
-      , VoteImageHandle: dmz.defs.createNamedHandle("vote_image")
-      , CalendarImageHandle: dmz.defs.createNamedHandle("calendar_image")
-      , ForumTimeHandle: dmz.defs.createNamedHandle("forum_time")
+        { ActiveHandle: dmz.defs.createNamedHandle("Active")
+        , AdminHandle: dmz.defs.createNamedHandle("admin_user")
+        , BioHandle: dmz.defs.createNamedHandle("bio")
+        , CommentHandle: dmz.defs.createNamedHandle("comment")
+        , CreatedByHandle: dmz.defs.createNamedHandle("created_by")
+        , DisplayNameHandle: dmz.defs.createNamedHandle("display_name")
+        , DurationHandle: dmz.defs.createNamedHandle("duration")
+        , ID: dmz.defs.createNamedHandle("id")
+        , NameHandle: dmz.defs.createNamedHandle("name")
+        , ParentHandle: dmz.defs.createNamedHandle("parent")
+        , PictureHandle: dmz.defs.createNamedHandle("picture")
+        , SentHandle: dmz.defs.createNamedHandle("sent")
+        , TextHandle: dmz.defs.createNamedHandle("text")
+        , TitleHandle: dmz.defs.createNamedHandle("title")
+        , VisibleHandle: dmz.defs.createNamedHandle("visible")
+        , PositionHandle: dmz.defs.createNamedHandle("position")
+        , TotalHandle: dmz.defs.createNamedHandle("total")
+        , ExpireHandle: dmz.defs.createNamedHandle("expire")
+        , ObjectHandle: dmz.defs.createNamedHandle("objectHandle")
 
-     , PositionHandle: dmz.defs.createNamedHandle("position")
-     , GroupPinHandle: dmz.defs.createNamedHandle("group_pin")
-     , ObjectHandle: dmz.defs.createNamedHandle("objectHandle")
-//      , DisabledHandle: dmz.defs.createNamedHandle("disabled")
-//      , PinCountHandle: dmz.defs.createNamedHandle("pin_count")
-//      , PinActiveHandle: dmz.defs.createNamedHandle("Pin_Active")
-//      , PinIDHandle: dmz.defs.createNamedHandle("pinID")
-//      , PinTitleHandle: dmz.defs.createNamedHandle("pinTitle")
-//      , PinDescHandle: dmz.defs.createNamedHandle("pinDescription")
-//      , PinFileHandle: dmz.defs.createNamedHandle("pinFile")
-//      , PinGroupCountHandle: dmz.defs.createNamedHandle("pinGroupCountHandle")
+        // Object-specific handles
+        , ForumTimeHandle: dmz.defs.createNamedHandle("forum_time")
+        , VoteState: dmz.defs.createNamedHandle("vote_state")
+        , EmailRecipientHandle: dmz.defs.createNamedHandle("email_recipient")
 
+        // Link Attr Handles
+        , AdvisorGroupHandle: dmz.defs.createNamedHandle("advisor_group")
+        , ForumLink: dmz.defs.createNamedHandle("group_forum_link")
+        , GameGroupHandle: dmz.defs.createNamedHandle("game_group")
+        , GameUngroupedUsersHandle: dmz.defs.createNamedHandle("game_ungrouped_users")
+        , GroupMembersHandle: dmz.defs.createNamedHandle("group_members")
+        , GroupPinHandle: dmz.defs.createNamedHandle("group_pin")
+        , VoteLinkHandle: dmz.defs.createNamedHandle("vote_link")
+        , QuestionLinkHandle: dmz.defs.createNamedHandle("question_link")
+        , MediaHandle: dmz.defs.createNamedHandle("game_media")
 
-      , VoteLinkHandle: dmz.defs.createNamedHandle("vote_link")
-      , QuestionLinkHandle: dmz.defs.createNamedHandle("question_link")
-      , VoteState: dmz.defs.createNamedHandle("vote_state")
-      , TotalHandle: dmz.defs.createNamedHandle("total")
-      , ExpireHandle: dmz.defs.createNamedHandle("expire")
+        // Time handles, and handles to be removed later
+        , UpdateTimeHandle: dmz.defs.createNamedHandle("update_time_handle")
+        , CreatedAtServerTimeHandle: dmz.defs.createNamedHandle("created_at_server_time")
+        , CreatedAtGameTimeHandle: dmz.defs.createNamedHandle("created_at_game_time")
+        , GameStartTimeHandle: dmz.defs.createNamedHandle("game_start_time")
+        , InGameStartTimeHandle: dmz.defs.createNamedHandle("in_game_start_time")
+        , ServerTimeHandle: dmz.defs.createNamedHandle("server_time")
 
-      , MediaHandle: dmz.defs.createNamedHandle("game_media")
-
-      /* Time handles, and handles to be removed later */
-      , UpdateTimeHandle: dmz.defs.createNamedHandle("update_time_handle")
-      , CreatedAtServerTimeHandle: dmz.defs.createNamedHandle("created_at_server_time")
-      , CreatedAtGameTimeHandle: dmz.defs.createNamedHandle("created_at_game_time")
-      , GameStartTimeHandle: dmz.defs.createNamedHandle("game_start_time")
-      , InGameStartTimeHandle: dmz.defs.createNamedHandle("in_game_start_time")
-      , ServerTimeHandle: dmz.defs.createNamedHandle("server_time")
-      }
+        // Group Office Image handles
+        , BackgroundImageHandle: dmz.defs.createNamedHandle("background_image")
+        , MapImageHandle: dmz.defs.createNamedHandle("map_image")
+        , ComputerImageHandle: dmz.defs.createNamedHandle("computer_image")
+        , TVImageHandle: dmz.defs.createNamedHandle("tv_image")
+        , NewspaperImageHandle: dmz.defs.createNamedHandle("newspaper_image")
+        , InboxImageHandle: dmz.defs.createNamedHandle("inbox_image")
+        , PhoneImageHandle: dmz.defs.createNamedHandle("phone_image")
+        , AdvisorImageHandle: dmz.defs.createNamedHandle("advisor_image")
+        , AdvisorImageCountHandle: dmz.defs.createNamedHandle("advisor_image_count")
+        , ExitImageHandle: dmz.defs.createNamedHandle("exit_image")
+        , ResourceImageHandle: dmz.defs.createNamedHandle("resource_image")
+        , VoteImageHandle: dmz.defs.createNamedHandle("vote_image")
+        , CalendarImageHandle: dmz.defs.createNamedHandle("calendar_image")
+        }
 
    , Functions =
-      { getDisplayName: false
-      , getAuthorName: false
-      , getAuthorHandle: false
-      , getUserGroupHandle: false
-      , addUITextLimit: false
-      , getVoteStatus: false
-      }
+        { getDisplayName: false
+        , getAuthorName: false
+        , getAuthorHandle: false
+        , getUserGroupHandle: false
+        , addUITextLimit: false
+        , getVoteStatus: false
+        }
 
    , getDisplayName
    , getAuthorHandle
