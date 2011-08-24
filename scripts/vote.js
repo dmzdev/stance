868d219b0bfb522273ef443278d60dc3995aae9e
Vladimir Kozyrev <kozyrevvladimir@gmail.com>
2011-08-24 10:33:07 -0700

    - Added more code.

--- scripts/vote.js
+++ scripts/vote.js
@@ -41,16 +41,146 @@ var dmz =
    , AvatarDefault = dmz.ui.graph.createPixmap(dmz.resources.findFile("AvatarDefault"))
    , hil
    , userGrouphandle
+   , isAdmin
 
    //Functions
    , openWindow
    , initiateVoteUI
-   , updateYesVotes
-   , updateNoVotes
+   , setYesNoLabels
+   , setActiveLabels
+   , setApprovalPendingLabels
+   , setDeniedLabels
+   , updateVotes
    , updateState
    ;
 
 LoginSkippedMessage.subscribe(self, function (data) { LoginSkipped = true; });
+
+updateState = function (voteHandle) {
+
+};
+
+updateVotes = function (voteHandle) {
+
+};
+
+setDeniedLabels = function (voteHandle) {
+
+};
+
+setApprovalPendingLabels = function (voteHandle) {
+
+};
+
+setActiveLabels = function (voteHandle) {
+
+};
+
+setYesNoLabels = function (voteHandle) {
+
+   var voteItem
+     , decisionItem
+     , pic
+     ;
+
+   if (VoteObjects[voteHandle] && VoteObjects[voteHandle].ui &&
+      VoteObjects[voteHandle].decisionHandle &&
+      DecisionObjects[VoteObjects[voteHandle].dicisionHandle]) {
+
+      voteItem = VoteObjects[voteHandle];
+      decisionItem = DecisionObjects[VoteObjects[voteHandle].dicisionHandle];
+
+      if (voteItem.state === VOTE_NO) {
+
+         voteItem.postItem.styleSheet("* { background-color: rgb(240, 70, 70); }");
+      }
+      else if (voteItem.state === VOTE_YES) {
+
+         voteItem.postItem.styleSheet("* { background-color: rgb(70, 240, 70); }");
+      }
+      if (voteItem.userPicture) {
+
+         pic = dmz.ui.graph.createPixmap(dmz.resources.findFile(voteItem.userPicture));
+         voteItem.ui.userPictureLabel.pixmap(pic);
+      }
+      if (voteItem.postedBy) {
+
+         voteItem.ui.postedByLabel.text("<b>Posted By: </b>" + voteItem.postedBy);
+      }
+      if (voteItem.question) {
+
+         voteItem.ui.questionLabel.text("<b>Question: </b>" + voteItem.question);
+      }
+      if (voteItem.state) {
+
+         voteItem.ui.stateLabel.text(
+            "<b>Vote status: </b>" +
+            dmz.stance.STATE_STR[voteItem.state]);
+      }
+   }
+};
+
+initiateVoteUI = function (voteHandle) {
+
+   var voteItem;
+
+   if (VoteObjects[voteHandle] && !VoteObjects[voteHandle].ui &&
+      (VoteObjects.state !== undefined)) {
+
+      voteItem = VoteObjects[voteHandle];
+      voteItem.ui.postItem = dmz.ui.loader.load("VoteViewPost");
+      voteItem.ui.userPictureLabel = voteItem.ui.postItem.lookup("userPictureLabel");
+      voteItem.ui.postedByLabel = voteItem.ui.postItem.lookup("postedByLabel");
+      voteItem.ui.startTimeLabel = voteItem.ui.postItem.lookup("startTimeLabel");
+      voteItem.ui.endTimeLabel = voteItem.ui.postItem.lookup("endTimeLabel");
+      voteItem.ui.questionLabel = voteItem.ui.postItem.lookup("questionLabel");
+      voteItem.ui.stateLabel = voteItem.ui.postItem.lookuo("stateLabel");
+      voteItem.ui.yesVotesLabel = voteItem.ui.postItem.lookup("yesVotesLabel");
+      voteItem.ui.noVotesLabel = voteItem.ui.postItem.lookup("noVotesLabel");
+      voteItem.ui.undecidedVotesLabel = voteItem.ui.postItem.lookup("undecidedVotesLabel");
+      voteItem.ui.advisorPictureLabel = voteItem.ui.postItem.lookup("advisorPictureLabel");
+      voteItem.ui.advisorReasonLabel = voteItem.ui.postItem.lookup("advisorReasonLabel");
+      voteItem.ui.yesButton = dmz.ui.button.createPushButton("Approve");
+      voteItem.ui.noButton = dmz.ui.button.createPushButton("Deny");
+      voteItem.ui.buttonLayout = dmz.postItem.lookup("buttonLayout");
+      voteItem.ui.textLayout = dmz.postItem.lookup("textLayout");
+      voteItem.ui.timeBox = dmz.ui.spinBox.createSpinBox("timeBox");
+      voteItem.ui.timeBox.minimum(24);
+      voteItem.ui.timeBox.maximum(72);
+      voteItem.ui.timeBox.setSingleStep(24);
+      voteItem.ui.timeBox.setSuffix("hrs");
+      voteItem.decisionReason = dmz.ui.textEdit.create("");
+      voteItem.decisionReason.sizePolicy(7, 0);
+      voteItem.decisionReason.fixedHeight(90);
+      voteItem.decisionReasonLabel = dmz.ui.label.create("<b>Decision Reason: </b>");
+
+      if ((voteItem.state === VOTE_NO) || (voteItem.state === VOTE_YES)) {
+
+         setYesNoLabels(voteHandle);
+      }
+      else if (voteItem.state === VOTE_ACTIVE) { setActiveLabels(voteHandle); }
+      else if (voteItem.state === VOTE_DENIED) { setDeniedLabels(voteHandle); }
+      else if (voteItem.state === VOTE_APPROVAL_PENDING) {
+
+         setApprovalPendingLabels(voteHandle);
+      }
+   }
+};
+
+openWindow = function () {
+
+};
+
+dmz.object.flag.observe(self, dmz.object.HILAttribute,
+function (objHandle, attrHandle, value) {
+
+   if (value) {
+
+      hil = objHandle;
+      userGrouphandle = dmz.stance.getUserGroupHandle(hil);
+      isAdmin = dmz.object.flag(hil, dmz.stance.AdminHandle);
+   }
+});
 
 dmz.object.create.observe(self, function (objHandle, objType) {
 
